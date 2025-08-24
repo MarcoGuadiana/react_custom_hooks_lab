aa4b85be36a61cdfa5c3c99de627a5ea72b4fb91
Marco Guadiana <marcoguadiana01@gmail.com>
2025-08-23 17:19:36 -0700

    fully functional

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,19 @@
-import { useEffect, useState } from "react";
+
+import { useState, useEffect } from "react";
 
 export function useLocalStorage(key, initialValue = null) {
+  
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key);
+    if (storedValue !== null) return storedValue; // <-- return string directly
+    return initialValue;
+  });
+
+  
+  useEffect(() => {
+    if (value === null || value === undefined) return;
+    localStorage.setItem(key, value); 
+  }, [key, value]);
+
+  return [value, setValue]; 
 }
